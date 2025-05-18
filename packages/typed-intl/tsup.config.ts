2ecb650f27826628a5c760738832e8b3f20043e8
EphraimHaber <ephraimxyz@gmail.com>
2025-05-18 19:36:15 +0300

    initial commit

---
+++ packages/typed-intl/tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  target: 'es2020',
+  format: ['cjs', 'esm'],
+  entry: ['src/index.ts'],
+  splitting: false,
+  sourcemap: true,
+  dts: true,
+});
