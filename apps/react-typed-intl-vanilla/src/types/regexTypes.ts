75dc93a0ad17f2e1886d173f65bc7b198f977af7
Elyasaf Elbaz <47217505+elyasaf755@users.noreply.github.com>
2025-05-23 14:14:32 +0300

    regexTypes | TrimRight, TrimLeft, Trim

--- apps/react-typed-intl-vanilla/src/types/regexTypes.ts
+++ apps/react-typed-intl-vanilla/src/types/regexTypes.ts
@@ -1,9 +1,23 @@
 export type ZeroOrMoreSpaces<S extends string> = S extends ""
-  ? S
-  : S extends ` ${infer Rest}` ? ` ${ZeroOrMoreSpaces<Rest>}` : never;
+    ? S
+    : S extends ` ${infer Rest}` ? ` ${ZeroOrMoreSpaces<Rest>}` : never;
 
 export type OneOrMoreSpaces<S extends string> = S extends ""
-  ? never
-  : S extends " "
-  ? S
-  : S extends ` ${infer Rest}` ? ` ${OneOrMoreSpaces<Rest>}` : never;
+    ? never
+    : S extends " "
+    ? S
+    : S extends ` ${infer Rest}` ? ` ${OneOrMoreSpaces<Rest>}` : never;
+
+export type TrimRight<S extends string> = S extends ""
+    ? S
+    : S extends " "
+    ? ""
+    : S extends `${infer Prefix} ` ? TrimRight<Prefix> : S;
+
+export type TrimLeft<S extends string> = S extends ""
+    ? S
+    : S extends " "
+    ? ""
+    : S extends ` ${infer Prefix}` ? TrimLeft<Prefix> : S;
+
+export type Trim<S extends string> = TrimRight<TrimLeft<S>>;
